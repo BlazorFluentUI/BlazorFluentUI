3edd2c056b1feef7f099dcc8368f34cc30aed039
Lee McPherson <leemcpherson@hotmail.com>
2019-06-06 08:50:29 -0700

    Nav getting there... need to redo button

---
+++ src/BlazorFabric.Nav/INavItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorFabric.Nav
+{
+    public interface INavItem
+    {
+        string Name { get; set; }
+        string Icon { get; set; }
+        string Link { get; set; }
+    }
+}
--- src/BlazorFabric.Nav/NavFabricLinkBase.cs
+++ src/BlazorFabric.Nav/NavFabricLinkBase.cs
@@ -8,7 +8,9 @@ using System.Threading.Tasks;
 namespace BlazorFabric.Nav
 {
     public class NavFabricLinkBase : FabricComponentBase
-    {
+    { 
+        [Parameter] protected int Context { get; set; }
+        
         [Parameter] protected RenderFragment ChildContent { get; set; }  //LINKS
 
         [Parameter] protected string AriaLabel { get; set; }
@@ -16,28 +18,59 @@ namespace BlazorFabric.Nav
         [Parameter] protected bool ForceAnchor { get; set; }
         [Parameter] protected string Icon { get; set; }
         [Parameter] protected bool IsButton { get; set; }
-        [Parameter] protected bool IsExpanded { get; set; }
         [Parameter] protected string Name { get; set; }
         [Parameter] protected string Target { get; set; }  //link <a> target
         [Parameter] protected string Title { get; set; } //tooltip and ARIA
-        [Parameter] protected string Url { get; set; }  
-                
+        [Parameter] protected string Url { get; set; }
+
+        [Parameter] protected int NestedDepth { get; set; }
+
         [Parameter] EventCallback<NavFabricLinkBase> OnClick { get; set; }
 
-        protected bool isSelected { get; set; }
+        protected bool isExpanded { get; set; }
 
+
+        protected bool isSelected { get; set; }
+        protected string depthClass = "";
+       
         protected override Task OnInitAsync()
         {
+            
             return base.OnInitAsync();
         }
 
         protected override Task OnParametersSetAsync()
         {
+            switch (this.NestedDepth)
+            {
+                case 0:
+                    depthClass = "";
+                    break;
+                case 1:
+                    depthClass = "depth-one";
+                    break;
+                case 2:
+                    depthClass = "depth-two";
+                    break;
+                case 3:
+                    depthClass = "depth-three";
+                    break;
+                case 4:
+                    depthClass = "depth-four";
+                    break;
+                case 5:
+                    depthClass = "depth-five";
+                    break;
+                case 6:
+                    depthClass = "depth-six";
+                    break;
+            }
             return base.OnParametersSetAsync();
         }
 
         protected async Task ClickHandler(UIMouseEventArgs args)
         {
+            this.isExpanded = !this.isExpanded;
             await OnClick.InvokeAsync(this);
         }
     }

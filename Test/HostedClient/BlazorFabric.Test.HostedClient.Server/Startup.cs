1c64d6ab3365b52c72f10976949fa78592b1e1b7
Lee McPherson <leemcpherson@hotmail.com>
2019-09-16 13:44:48 -0700

    Added workaround for firewalls blocking dll files

--- Test/HostedClient/BlazorFabric.Test.HostedClient.Server/Startup.cs
+++ Test/HostedClient/BlazorFabric.Test.HostedClient.Server/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
@@ -25,6 +26,13 @@ namespace BlazorFabric.Test.HostedClient.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var options = new RewriteOptions()
+                .AddRewrite("^_framework/_bin/(.*)\\.blazor", "_framework/_bin/$1.dll",
+             skipRemainingRules: true);
+
+            app.UseRewriter(options);
+
+
             app.UseResponseCompression();
 
             if (env.IsDevelopment())

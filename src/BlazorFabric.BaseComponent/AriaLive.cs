b9c8632017df6890a780060a73efcb23c3833234
Lee McPherson <leemcpherson@hotmail.com>
2019-10-14 12:23:14 -0700

    Added spinner!

---
+++ src/BlazorFabric.BaseComponent/AriaLive.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorFabric
+{
+    public enum AriaLive
+    {
+        Assertive,
+        Polite,
+        Off
+    }
+}
---
+++ src/BlazorFabric.Spinner/SpinnerBase.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorFabric
+{
+    public class SpinnerBase : FabricComponentBase
+    {
+        [Parameter] public string AriaLabel { get; set; }
+        [Parameter] public AriaLive AriaLive { get; set; } = AriaLive.Polite;
+        [Parameter] public string Label { get; set; }
+        [Parameter] public SpinnerLabelPosition LabelPosition { get; set; } = SpinnerLabelPosition.Bottom;
+        [Parameter] public SpinnerSize Size { get; set; } = SpinnerSize.Medium;
+        [Parameter] public string StatusMessage { get; set; }
+
+        protected string GetAriaLive()
+        {
+            switch (AriaLive)
+            {
+                case AriaLive.Polite:
+                    return "polite";
+                case AriaLive.Assertive:
+                    return "assertive";
+                case AriaLive.Off:
+                    return "off";
+                default:
+                    return "polite";
+            }
+        }
+
+        protected string GetPositionStyle()
+        {
+            switch (LabelPosition)
+            {
+                case SpinnerLabelPosition.Left:
+                    return " ms-Spinner--left";
+                case SpinnerLabelPosition.Right:
+                    return " ms-Spinner--right";
+                case SpinnerLabelPosition.Top:
+                    return " ms-Spinner--top";
+                default:
+                    return "";
+            }
+        }
+
+        protected string GetSpinnerSizeStyle()
+        {
+            switch (Size)
+            {
+                case SpinnerSize.Large:
+                    return " ms-Spinner--large";
+                case SpinnerSize.Medium:
+                    return " ms-Spinner--medium";
+                case SpinnerSize.Small:
+                    return " ms-Spinner--small";
+                case SpinnerSize.XSmall:
+                    return " ms-Spinner--xSmall";
+                default:
+                    return " ms-Spinner--medium";
+            }
+        }
+
+    }
+}

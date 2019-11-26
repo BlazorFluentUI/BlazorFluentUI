866407d826bc3d35f07dee27f8a2448e2ca82095
Lee McPherson <leemcpherson@hotmail.com>
2019-11-26 13:36:01 -0800

    Added FocusZone to OverflowSet and added vertical OverflowSet to demo

--- src/BlazorFabric.OverflowSet/OverflowSetBase.cs
+++ src/BlazorFabric.OverflowSet/OverflowSetBase.cs
@@ -16,12 +16,16 @@ namespace BlazorFabric
 
         [Parameter] public RenderFragment<IEnumerable<TItem>> OverflowTemplate { get; set; }
 
+        [Parameter] public bool DoNotContainWithinFocusZone { get; set; }
+
         //[Parameter] public RenderFragment<RenderFragment> OverflowMenuButtonTemplate { get; set; }
 
         [Parameter] public Func<TItem, string> GetKey { get; set; }
 
         //protected System.Collections.Generic.List<TItem> calculatedItems;
         //protected System.Collections.Generic.List<TItem> calculatedOverflowItems;
+
+        protected FocusZoneBase focusZoneComponent;
 
         protected override Task OnParametersSetAsync()
         {

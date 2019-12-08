518b26906907949b265b2fab358669bd794c8324
Eweol <eweol@outlook.de>
2019-12-08 20:02:52 +0100

    add base of Text-Component to BlazorFabric

---
+++ src/BlazorFabric.BaseComponent/TextType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorFabric
+{
+    public enum TextType
+    {
+        Tiny,
+        XSmall,
+        Small,
+        SmallPlus,
+        Medium,
+        MediumPlus,
+        Large,
+        XLarge,
+        XxLarge,
+        Mega
+    }
+
+    public static partial class CssUtils
+    {
+        public static Dictionary<TextType, string> TextTypeMap = new Dictionary<TextType, string>
+        {
+            [TextType.Tiny] = "tiny",
+            [TextType.XSmall] = "xSmal",
+            [TextType.Small] = "small",
+            [TextType.SmallPlus] = "smallPlus",
+            [TextType.Medium] = "medium",
+            [TextType.MediumPlus] = "mediumPlus",
+            [TextType.Large] = "large",
+            [TextType.XLarge] = "xLarge",
+            [TextType.XxLarge] = "xxLarge",
+            [TextType.Mega] = "mega"
+        };
+    }
+}

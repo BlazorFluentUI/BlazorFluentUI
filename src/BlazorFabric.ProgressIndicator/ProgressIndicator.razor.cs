f42fda2fb7a2a5769d43d88da4be885a77c26e6b
Eweol <eweol@outlook.de>
2019-12-08 12:58:52 +0100

    correct comment

--- src/BlazorFabric.ProgressIndicator/ProgressIndicator.razor.cs
+++ src/BlazorFabric.ProgressIndicator/ProgressIndicator.razor.cs
@@ -61,7 +61,7 @@ namespace BlazorFabric
 
             if (_percent >= 0)
             {
-                // Replace ',' with '.' to support also comma seperated percatage values and prevent jump back to 0
+                // Replace ',' with '.' to support also comma percatage values and prevent jump back to 0
                 styles += $"width: {_percent.ToString().Replace(',','.')}%;";
                 if (PercentComplete < ZERO_THRESHOLD)
                 {

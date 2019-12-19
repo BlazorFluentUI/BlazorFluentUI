00f21f18d07683687b3b7b5366d02c575cae7e82
Lee McPherson <leemcpherson@hotmail.com>
2019-12-19 13:10:39 -0800

    Added PrefixContent and SuffixContent on TextField so that you can add custom content other than plain text.

--- src/BlazorFabric.TextField/TextField.razor.cs
+++ src/BlazorFabric.TextField/TextField.razor.cs
@@ -24,7 +24,9 @@ namespace BlazorFabric
         [Parameter] public RenderFragment RenderLabel { get; set; }
         [Parameter] public string Description { get; set; }
         [Parameter] public string Prefix { get; set; }
+        [Parameter] public RenderFragment PrefixContent { get; set; }
         [Parameter] public string Suffix { get; set; }
+        [Parameter] public RenderFragment SuffixContent { get; set; }
         [Parameter] public string DefaultValue { get; set; }
         [Parameter] public string Value { get; set; }
         [Parameter] public bool Disabled { get; set; }

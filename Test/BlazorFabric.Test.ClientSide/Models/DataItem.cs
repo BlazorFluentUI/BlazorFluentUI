10d4c1f6293629eeb868463c6978c3f819962022
Lee McPherson <leemcpherson@hotmail.com>
2019-12-29 22:12:58 -0800

    better client-side demo for github pages

---
+++ Test/BlazorFabric.Test.ClientSide/Models/DataItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorFabric.Test.ClientSide.Models
+{
+    public class DataItem
+    {
+        public DataItem(int num)
+        {
+            DisplayName = num.ToString();
+        }
+
+        public DataItem(string text)
+        {
+            DisplayName = text;
+        }
+
+        public DataItem(string text, SelectableOptionMenuItemType selectableOptionMenuItemType)
+        {
+            DisplayName = text;
+            Type = selectableOptionMenuItemType;
+        }
+        public string DisplayName { get; set; }
+        public string ImgUrl => "/background.png";
+
+        public SelectableOptionMenuItemType Type { get; set; }
+    }
+}

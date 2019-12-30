739085e6ea56e48d0591cfa6e13099875e6bde99
Eweol <eweol@outlook.de>
2019-12-30 19:34:26 +0100

    finish up refactoring and reorganize DemoApplications

---
+++ Demo/BlazorFabric.Demo.ClientSide/Startup.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+namespace BlazorFabric.Demo.ClientSide
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBlazorFabric();
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.UseLocalTimeZone();
+            app.AddComponent<App>("app");
+        }
+    }
+}

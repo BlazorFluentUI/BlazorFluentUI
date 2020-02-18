357705a44e68d1842ae0a9b2466b46f48f098f3a
Eweol <eweol@outlook.de>
2020-02-18 20:29:26 +0100

    add ObservableExtension again because deletion was wrong Add ability to set styles Fix to head tag also if all related components are disposed

---
+++ src/BlazorFabric.BaseComponent/Extensions/ObservableExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+using System.Text;
+
+namespace BlazorFabric
+{
+    public static class ObservableExtensions
+    {
+        //public static IObservable<T> SampleFirst<T>(
+        //    this IObservable<T> source,
+        //    TimeSpan sampleDuration,
+        //    IScheduler scheduler = null)
+        //{
+        //    scheduler = scheduler ?? Scheduler.Default;
+        //    return source.Publish(ps =>
+        //        ps.Window(() => ps.Delay(sampleDuration, scheduler))
+        //          .SelectMany(x => x.Take(1)));
+        //}
+
+        public static IObservable<T> SampleFirst<T>(
+            this IObservable<T> source,
+            TimeSpan timeout)
+        {
+            return source.Window(() => { return Observable.Interval(timeout); })
+                        .SelectMany(x => x.Take(1));
+        }
+    }
+}

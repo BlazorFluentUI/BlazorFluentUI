46c485f981629d136db76897804dcfbd163c5f3d
Lee McPherson <leemcpherson@hotmail.com>
2020-03-07 11:14:24 -0800

    Layer converted to new style

--- src/BlazorFabric.Layer/LayerHost.razor.cs
+++ src/BlazorFabric.Layer/LayerHost.razor.cs
@@ -14,7 +14,7 @@ namespace BlazorFabric
         [Parameter] public RenderFragment HostedContent { get; set; }
 
         [Parameter] public bool IsFixed { get; set; } = true;
-        
+
         protected LayerPortalGenerator portalGeneratorReference;
 
         public void AddOrUpdateHostedContent(string layerId, RenderFragment renderFragment, string style)
@@ -25,6 +25,39 @@ namespace BlazorFabric
         public void RemoveHostedContent(string layerId)
         {
             portalGeneratorReference.RemoveHostedContent(layerId);
+        }
+
+        private ICollection<Rule> CreateGlobalCss()
+        {
+            var layerRules = new HashSet<Rule>();
+            // ROOT
+            layerRules.Add(new Rule()
+            {
+                Selector = new CssStringSelector() { SelectorName = ".ms-Layer--fixed" },
+                Properties = new CssString()
+                {
+                    Css = $"position:fixed;"+
+                          $"z-index:{Theme.ZIndex.Layer};"+
+                          $"top:0;"+
+                          $"left:0;" +
+                          $"width:100vw;" +
+                          $"height:100vh;" +
+                          $"visibility:hidden;"
+                }
+            });
+
+            layerRules.Add(new Rule()
+            {
+                Selector = new CssStringSelector() { SelectorName = ".ms-Layer-content" },
+                Properties = new CssString()
+                {
+                    Css = $"visibility:visible;"
+                }
+            });
+
+
+
+            return layerRules;
         }
 
     }

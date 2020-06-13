9c046985c252889887202cf64467b48bd79c55e8
Lee McPherson <leemcpherson@hotmail.com>
2020-06-13 13:05:11 -0700

    Last change to Dropdown... not needed for packages immediately, but removes Key property from change args.  The Key argument won't work anymore anyways.

--- src/BlazorFluentUI.BFUDropdown/BFUDropdownChangeArgs.cs
+++ src/BlazorFluentUI.BFUDropdown/BFUDropdownChangeArgs.cs
@@ -6,15 +6,15 @@ namespace BlazorFluentUI
 {
     public class BFUDropdownChangeArgs
     {
-        public IBFUDropdownOption? Option { get; set; }
-        [Obsolete] public string? Key { get; set; } 
+        public IBFUDropdownOption Option { get; set; }
+        //[Obsolete] public string? Key { get; set; } 
         public bool IsAdded { get; set; }
 
-        public BFUDropdownChangeArgs(string key, bool isAdded)
-        {
-            Key = key;
-            IsAdded = isAdded;
-        }
+        //public BFUDropdownChangeArgs(string key, bool isAdded)
+        //{
+        //    Key = key;
+        //    IsAdded = isAdded;
+        //}
 
         public BFUDropdownChangeArgs(IBFUDropdownOption option, bool isAdded)
         {

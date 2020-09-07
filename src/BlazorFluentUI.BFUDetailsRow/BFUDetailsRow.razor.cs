d297c2daaee976bcf9f4289cde133943f614cfe4
Lee McPherson <leemcpherson@hotmail.com>
2020-09-07 09:33:50 -0700

    small changes to make everything compile properly.  Multiple copies of a typings file was interfering with typescript compilation.

--- src/BlazorFluentUI.BFUDetailsRow/BFUDetailsRow.razor.cs
+++ src/BlazorFluentUI.BFUDetailsRow/BFUDetailsRow.razor.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorFluentUI.Style;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;

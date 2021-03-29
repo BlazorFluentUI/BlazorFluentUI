78770b27f1740a7bd28899a5814b8410fe37b927
Lee McPherson <leemcpherson@hotmail.com>
2021-03-29 16:49:35 -0700

    Fixes problems with twoway binding inside a custom cell of DetailsRow.  Output is now a DynamicAccessor<object>.  Added demo to DetailsList page.  Performance is not great when editing contents of DetailsList cell.

---
+++ src/BlazorFluentUI.CoreComponents/DetailsRow/DynamicAccessor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFluentUI
+{
+    public class DynamicAccessor<TItem>
+    {
+        public object Value
+        {
+            get => _getter(_item);
+            set => _setter(_item, value);
+        }
+
+        private Func<TItem, object> _getter;
+        private Action<TItem, object> _setter;
+        private TItem _item;
+
+        public DynamicAccessor(TItem item, Expression<Func<TItem,object>> getter)
+        {
+            _item = item;
+            _getter = getter.Compile();
+            _setter = DetailsRowUtils.GetSetter(getter);
+        }
+    }
+}

9d770bb7c317cbb28aa6e1c45b9dd1e4fbd95410
Vincent Baaij <vnbaaij@outlook.com>
2021-06-03 09:46:24 +0200

    Remove commented out code

--- src/BlazorFluentUI.CoreComponents/ChoiceGroup/ChoiceGroupOption.razor.cs
+++ src/BlazorFluentUI.CoreComponents/ChoiceGroup/ChoiceGroupOption.razor.cs
@@ -52,60 +52,6 @@ namespace BlazorFluentUI
                 Id = $"id_{Guid.NewGuid().ToString().Replace("-","")}";
         }
 
-        //private void AddFieldHoverOrFocusStyles(HashSet<IRule> rules, ITheme theme, string pseudoSelector)
-        //{
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled) .ms-ChoiceField-field:{pseudoSelector} .ms-ChoiceFieldLabel").AppendCssStyles(
-        //       $"color:{theme.Palette.NeutralDark}");
-
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled) .is-checked .ms-ChoiceField-field:{pseudoSelector}:before").AppendCssStyles(
-        //        $"border-color:{theme.Palette.ThemeDark}");
-
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled):not(.is-checked) .ms-ChoiceField-field:{pseudoSelector}:before").AppendCssStyles(
-        //        $"border-color:var(--semanticColors.InputBorderHovered}");
-
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled):not(.is-checked):not(.custom-content) .ms-ChoiceField-field:{pseudoSelector}:after").AppendCssStyles(
-        //        "content:''",
-        //        "transition-property:background-color",
-        //        "left:5px",
-        //        "top:5px",
-        //        "width:10px",
-        //        "height:10px",
-        //        $"background-color:{theme.Palette.NeutralSecondary}");
-
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled) .is-checked .ms-ChoiceField-field:{pseudoSelector}").AppendCssStyles(
-        //        $"border-color:{theme.Palette.ThemeDark}");
-        //}
-
-        //private void AddFieldWithCustomContentRules(HashSet<IRule> rules, ITheme theme, string pseudoSelector)
-        //{
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled).is-checked.custom-content .ms-ChoiceField-field:{pseudoSelector}").AppendCssStyles(
-        //      $"border-color:{theme.Palette.ThemeDark}");
-
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled):not(.is-checked).custom-content .ms-ChoiceField-field:{pseudoSelector}").AppendCssStyles(
-        //      $"border-color:var(--semanticColors.InputBorderHovered}");
-
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled) .is-checked.custom-content .ms-ChoiceField-field:{pseudoSelector}:before").AppendCssStyles(
-        //        "opacity:1",
-        //        $"border-color:{theme.Palette.ThemeDark}");
-
-        //    rules.AddCssStringSelector($".ms-ChoiceField:not(.is-disabled):not(.is-checked).custom-content .ms-ChoiceField-field:{pseudoSelector}:before").AppendCssStyles(
-        //        "opacity:1",
-        //        $"border-color:var(--semanticColors.InputBorderHovered}");
-        //}
-
-        //private void AddCustomContentWrapperStyle(HashSet<IRule> rules, ITheme theme)
-        //{
-        //    rules.AddCssStringSelector(".custom-content .ms-ChoiceField-customContentWrapper").AppendCssStyles(
-        //        "padding-bottom:2px",
-        //        "transition-property:opacity",
-        //        $"transition-duration:{_transitionDuration}",
-        //        "transition-timing-function:ease");
-
-        //    rules.AddCssStringSelector(".ms-ChoiceField-customContentWrapper .ms-Image").AppendCssStyles(
-        //        "display:inline-block",
-        //        "border-style:none");
-        //}
-
         private async Task OnOptionClick(MouseEventArgs mouseEventArgs)
         {
             if (!IsDisabled)

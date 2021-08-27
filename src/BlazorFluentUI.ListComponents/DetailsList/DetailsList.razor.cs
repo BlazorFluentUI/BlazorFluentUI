cf7ccaa4c0bc3ac40d358522d7f025ba799b454c
Vincent Baaij <vnbaaij@outlook.com>
2021-08-27 11:48:46 +0200

    Allow null for DetailList column value

--- src/BlazorFluentUI.ListComponents/DetailsList/DetailsList.razor.cs
+++ src/BlazorFluentUI.ListComponents/DetailsList/DetailsList.razor.cs
@@ -1,12 +1,11 @@
-﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.JSInterop;
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
 
 namespace BlazorFluentUI.Lists
 {

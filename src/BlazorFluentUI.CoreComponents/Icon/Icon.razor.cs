be8d5c0dad17c56a44659d4fc2d9f766a6bbfe20
Lee McPherson <leemcpherson@hotmail.com>
2021-09-13 08:18:19 -0700

    Converted FontIcon to internally use Icon and marked obsolete.  Added properties to Icon.  Fixed Checkbox styling

--- src/BlazorFluentUI.CoreComponents/Icon/Icon.razor.cs
+++ src/BlazorFluentUI.CoreComponents/Icon/Icon.razor.cs
@@ -4,9 +4,33 @@ namespace BlazorFluentUI
 {
     public partial class Icon : FluentUIComponentBase
     {
+        [Parameter] public bool Filled { get; set; }
         [Parameter] public string? IconName { get; set; }
+        [Parameter] public int IconSize { get; set; } = 24;
         [Parameter] public string? IconSrc { get; set; }
         [Parameter] public IconType IconType { get; set; }
+        [Parameter] public bool UseFluentUISystemIcons { get; set; } = true;
 
+        public string IconClassName
+        {
+            get
+            {
+                if (IconName == null)
+                {
+                    return "ms-Icon-placeHolder";
+                }
+                else
+                {
+                    if (!UseFluentUISystemIcons)
+                    {
+                        return IconName;
+                    }
+                    else
+                    {
+                        return $"icon-ic_fluent_{IconName}_{IconSize}_{(Filled ? "filled" : "regular")}";
+                    }
+                }
+            }
+        }
     }
 }
---
+++ src/BlazorFluentUI.CoreComponents/Icon/IconSize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFluentUI
+{
+    public enum IconSize
+    {
+        Size12 = 12,
+        Size16 = 16,
+        Size20 = 20,
+        Size24 = 24,
+        Size28 = 28,
+        Size32 = 32
+    }
+}

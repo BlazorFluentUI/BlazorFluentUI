fc09afb81605310cb73157e186d1964195e00115
Lee McPherson <leemcpherson@hotmail.com>
2021-09-15 12:08:38 -0700

    better font page

---
+++ Demo/BlazorFluentUI.Demo.Shared/Models/FontModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorFluentUI.Demo.Shared.Models
+{
+    internal class FontModel
+    {
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public bool Filled { get; set;  }
+    }
+}

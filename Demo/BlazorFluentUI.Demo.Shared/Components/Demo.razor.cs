16a1915dff2fa6cfb80238e3de6a205dbfc27cda
Vincent Baaij <vnbaaij@outlook.com>
2021-09-16 07:34:34 +0200

    rc1 update

--- Demo/BlazorFluentUI.Demo.Shared/Components/Demo.razor.cs
+++ Demo/BlazorFluentUI.Demo.Shared/Components/Demo.razor.cs
@@ -1,91 +1,78 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorFluentUI.Demo.Shared.Components
 {
-    public partial class Demo : ComponentBase
-    {
-        [Inject] ThemeProvider? ThemeProvider { get; set; }
-        //[Inject] HttpClient? HttpClient { get; set; }
+	public partial class Demo : ComponentBase
+	{
+		[Inject] ThemeProvider? ThemeProvider { get; set; }
 
-        ITheme Theme => ThemeProvider!.Theme;
+		ITheme Theme => ThemeProvider!.Theme;
 
-        string codeLabel = "Show Code";
-        bool showCode = false;
+		string codeLabel = "Show Code";
+		bool showCode = false;
 
-        bool showCCode = false;
+		bool showCCode = false;
 
-        string codeLiteral = "";
-        string ccodeLiteral = "";
+		string codeLiteral = "";
+		string ccodeLiteral = "";
 
-        [Parameter] public string? Header { get; set; }
+		[Parameter] public string? Header { get; set; }
 
-        [Parameter] public string? MetadataPath { get; set; }
+		[Parameter] public string? MetadataPath { get; set; }
 
-        [Parameter] public int Key { get; set; } = -1;//This is required and must be unique.
+		[Parameter] public int Key { get; set; } = -1;//This is required and must be unique.
 
-        [Parameter] public RenderFragment? ChildContent { get; set; }
+		[Parameter] public RenderFragment? ChildContent { get; set; }
 
-        protected override async Task OnInitializedAsync()
-        {
+		protected override async Task OnInitializedAsync()
+		{
+			if (Key == -1)
+				throw new Exception("Must set Key with an integer 0 or greater and must be unique within page.");
 
-            //HelloWorld.SayHello();
-            if (Key == -1)
-                throw new Exception("Must set Key with an integer 0 or greater and must be unique within page.");
+			string demoMetaData = BlazorFluentUI.Demo.DemoGenerated.GetRazor(MetadataPath!);
+			int start = 0;
+			int end = demoMetaData.Length;
+			bool found = false;
 
-            // Source inspired by AntDesign (https://github.com/Append-IT/ant-design-blazor/blob/master/docs/Append.AntDesign.Documentation/Infrastructure/DocumentationService.cs)
-            //string demoMetaData = await HttpClient!.GetStringAsync($"md/{MetadataPath}.md");
-            var demoMetaData = BlazorFluentUI.Demo.DemoGenerated.GetRazor(MetadataPath);
-            var start = 0;
-            var end = demoMetaData.Length;
-            bool found = false;
-            while ((start < end) && (!found))
-            {
-                int indexOfDemoBegin = demoMetaData.IndexOf("<Demo", start) + 6;
-                int keyValueBegin = demoMetaData.IndexOf("Key=\"", indexOfDemoBegin) + 5;
-                int keyValueEnd = demoMetaData.IndexOf("\"", keyValueBegin);
-                int keyValue = 0;
-                int.TryParse(demoMetaData.Substring(keyValueBegin, keyValueEnd - keyValueBegin), out keyValue);
+			while ((start < end) && (!found))
+			{
+				int indexOfDemoBegin = demoMetaData.IndexOf("<Demo", start) + 6;
+				int keyValueBegin = demoMetaData.IndexOf("Key=\"", indexOfDemoBegin) + 5;
+				int keyValueEnd = demoMetaData.IndexOf("\"", keyValueBegin);
 
-                if (keyValue == Key)
-                {
-                    found = true;
-                    //var codeBegin = demoMetaData.IndexOf(">", indexOfDemoBegin) + 1;
-                    var codeBegin = demoMetaData.IndexOf(Environment.NewLine, indexOfDemoBegin) + 1;
-                    var codeEnd = demoMetaData.IndexOf("</Demo>", indexOfDemoBegin);
-                    codeLiteral = demoMetaData.Substring(codeBegin, codeEnd - codeBegin);
-                    codeLiteral = codeLiteral.Replace("                ", "");
-                }
+				if (int.TryParse(demoMetaData[keyValueBegin..keyValueEnd], out int keyValue) && keyValue == Key)
+					found = true;
 
-                start = indexOfDemoBegin;
-            }
+				int codeBegin = demoMetaData.IndexOf(Environment.NewLine, indexOfDemoBegin) + 1;
+				int codeEnd = demoMetaData.IndexOf("</Demo>", indexOfDemoBegin);
+				codeLiteral = demoMetaData[codeBegin..codeEnd].Replace("                ", "");
+				start = codeEnd;
+			}
 
-            int ccodeIndex = demoMetaData.IndexOf("@code{");
-            if (ccodeIndex == -1)
-            {
-                ccodeIndex = demoMetaData.IndexOf("@code ");
-                if (ccodeIndex == -1)
-                {
-                    await base.OnParametersSetAsync();
-                    return; //can't find code block
-                }
-                ccodeIndex = demoMetaData.IndexOf("{", ccodeIndex) + 1;
-            }
-            else
-            {
-                ccodeIndex += 6;
-            }
-            var lastIndex = demoMetaData.LastIndexOf("}");
+			int ccodeIndex = demoMetaData.IndexOf("@code{");
+			if (ccodeIndex == -1)
+			{
+				ccodeIndex = demoMetaData.IndexOf("@code ");
+				if (ccodeIndex == -1)
+				{
+					await base.OnParametersSetAsync();
+					return; //can't find code block
+				}
+				ccodeIndex = demoMetaData.IndexOf("{", ccodeIndex) + 1;
+			}
+			else
+			{
+				ccodeIndex += 6;
+			}
+			var lastIndex = demoMetaData.LastIndexOf("}");
 
-            ccodeLiteral = "@code {\n" + demoMetaData.Substring(ccodeIndex, lastIndex - ccodeIndex) + "\n}";
+			ccodeLiteral = "@code {\n" + demoMetaData[ccodeIndex..lastIndex] + "\n}";
 
 
-            await base.OnInitializedAsync();
-        }
+			await base.OnInitializedAsync();
+		}
 
-    }
+	}
 }
--- src/BlazorFluentUI.CoreComponents/Slider/Slider.razor.cs
+++ src/BlazorFluentUI.CoreComponents/Slider/Slider.razor.cs
@@ -1,305 +1,308 @@
-﻿using System.Timers;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Timers;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using Timer = System.Timers.Timer;
 
 namespace BlazorFluentUI
 {
-	public partial class Slider : FluentUIComponentBase, IAsyncDisposable
-	{
-		[Parameter] public Func<double, string>? AriaValueText { get; set; }
-		[Parameter] public double? DefaultValue { get; set; }
-		[Parameter] public bool Disabled { get; set; } = false;
-		[Parameter] public string? Label { get; set; }
-		[Parameter] public string? LineContainerClassName { get; set; }
-		[Parameter] public double Max { get; set; } = 10.0;
-		[Parameter] public double Min { get; set; } = 0.0;
-		[Parameter] public bool OriginFromZero { get; set; } = false;
-		[Parameter] public bool ShowValue { get; set; } = true;
-		[Parameter] public bool SnapToStep { get; set; } = false;
-		[Parameter] public double Step { get; set; } = 1.0;
-		[Parameter] public string? TitleLabelClassName { get; set; }
-		[Parameter] public double? Value { get; set; }
-		[Parameter] public EventCallback<double> ValueChanged { get; set; }
-		[Parameter] public Func<double, string>? ValueFormat { get; set; }
-		[Parameter] public bool Vertical { get; set; }
+    public partial class Slider : FluentUIComponentBase, IAsyncDisposable
+    {
+        [Parameter] public Func<double, string>? AriaValueText { get; set; }
+        [Parameter] public double? DefaultValue { get; set; }
+        [Parameter] public bool Disabled { get; set; } = false;
+        [Parameter] public string? Label { get; set; }
+        [Parameter] public string? LineContainerClassName { get; set; }
+        [Parameter] public double Max { get; set; } = 10.0;
+        [Parameter] public double Min { get; set; } = 0.0;
+        [Parameter] public bool OriginFromZero { get; set; } = false;
+        [Parameter] public bool ShowValue { get; set; } = true;
+        [Parameter] public bool SnapToStep { get; set; } = false;
+        [Parameter] public double Step { get; set; } = 1.0;
+        [Parameter] public string? TitleLabelClassName { get; set; }
+        [Parameter] public double? Value { get; set; }
+        [Parameter] public EventCallback<double> ValueChanged { get; set; }
+        [Parameter] public Func<double, string>? ValueFormat { get; set; }
+        [Parameter] public bool Vertical { get; set; }
 
-		private readonly string id = $"id_{Guid.NewGuid().ToString().Replace("-", "")}";
-		private ElementReference slideBox;
-		private ElementReference sliderLine;
-		private ElementReference thumb;
-		private double zeroOffsetPercent;
-		private double thumbOffsetPercent;
-		private double _renderedValue;
-		private bool showTransitions;
-		private bool initialValueSet = false;
-		private double value;
+        private readonly string id = $"id_{Guid.NewGuid().ToString().Replace("-", "")}";
+        private ElementReference slideBox;
+        private ElementReference sliderLine;
+        private ElementReference thumb;
+        private double zeroOffsetPercent;
+        private double thumbOffsetPercent;
+        private double _renderedValue;
+        private bool showTransitions;
+        private bool initialValueSet = false;
+        private double value;
 
-		private DotNetObjectReference<Slider>? selfReference;
-		private Timer timer = new();
+        private DotNetObjectReference<Slider>? selfReference;
+        private Timer timer = new();
 
-		private const string ScriptPath = "./_content/BlazorFluentUI.CoreComponents/slider.js";
-		private IJSObjectReference? scriptModule;
+        private const string ScriptPath = "./_content/BlazorFluentUI.CoreComponents/slider.js";
+        private IJSObjectReference? scriptModule;
 
-		private string LengthString => (Vertical ? "height" : "width");
+        private string LengthString => (Vertical ? "height" : "width");
 
-		private static readonly Dictionary<string, string> GlobalClassNames = new()
-		{
-			{ "root", "ms-Slider" },
-			{ "enabled", "ms-Slider-enabled" },
-			{ "disabled", "ms-Slider-disabled" },
-			{ "row", "ms-Slider-row" },
-			{ "column", "ms-Slider-column" },
-			{ "container", "ms-Slider-container" },
-			{ "slideBox", "ms-Slider-slideBox" },
-			{ "line", "ms-Slider-line" },
-			{ "thumb", "ms-Slider-thumb" },
-			{ "activeSection", "ms-Slider-active" },
-			{ "inactiveSection", "ms-Slider-inactive" },
-			{ "valueLabel", "ms-Slider-value" },
-			{ "showValue", "ms-Slider-showValue" },
-			{ "showTransitions", "ms-Slider-showTransitions" },
-			{ "zeroTick", "ms-Slider-zeroTick" },
-			{ "titleLabel", "ms-Slider-titleLabel" },
-			{ "lineContainer", "ms-Slider-lineContainer" }
-		};
-		private bool shouldFocus;
-
-
-		private void UpdateState()
-		{
-			thumbOffsetPercent = Min == Max ? 0 : ((_renderedValue - Min) / (Max - Min)) * 100.0;
-
-			zeroOffsetPercent = Min >= 0 ? 0 : (-Min / (Max - Min)) * 100;
-			//lengthString = (set as property)
-			showTransitions = _renderedValue == value;
-			StateHasChanged();
-		}
-
-		protected override async Task OnInitializedAsync()
-		{
-			timer.Interval = 1000;
-			timer.AutoReset = false;
-			timer.Elapsed += Timer_Elapsed;
-			await base.OnInitializedAsync();
-		}
-
-		private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
-		{
-			_ = InvokeAsync(() => _ = ValueChanged.InvokeAsync(value));
-		}
-
-		protected override async Task OnParametersSetAsync()
-		{
-			if (Value.HasValue)
-			{
-				value = Value.Value;
-				_renderedValue = value;
-			}
-			else if (DefaultValue.HasValue && !initialValueSet)
-			{
-				initialValueSet = true;
-				value = DefaultValue.Value;
-				_renderedValue = value;
-			}
-
-			//if  (selfReference == null)
-			//    selfReference = DotNetObjectReference.Create(this);
-			//if (Disabled)
-			//    await baseModule!.InvokeVoidAsync("unregisterHandler", selfReference);
-			//else
-			//    await baseModule!.InvokeVoidAsync("registerMouseOrTouchStart", selfReference, slideBox, sliderLine);
-
-			UpdateState();
-
-			await base.OnParametersSetAsync();
-		}
-
-		protected override async Task OnAfterRenderAsync(bool firstRender)
-		{
-			if (scriptModule == null)
-				scriptModule = await JSRuntime!.InvokeAsync<IJSObjectReference>("import", ScriptPath);
-			selfReference = DotNetObjectReference.Create(this);
-
-			if (firstRender)
-			{
-				if (!Disabled)
-				{
-					await scriptModule.InvokeVoidAsync("registerMouseOrTouchStart", selfReference, slideBox, sliderLine);
-				}
-				else
-					await scriptModule.InvokeVoidAsync("unregisterHandlers", selfReference);
-			}
-
-			if (shouldFocus)
-			{
-				shouldFocus = false;
-				await Focus();
-			}
-
-			await base.OnAfterRenderAsync(firstRender);
-		}
-
-		[JSInvokable]
-		public void OnKeyDown(KeydownSliderArgs args)
-		{
-			double diff = 0;
-			double current = value;
-			if (args.Min)
-			{
-				current = Min;
-			}
-			else if (args.Max)
-			{
-				current = Max;
-			}
-			else
-			{
-				diff = args.Step;
-			}
-
-			double newValue = Math.Min(Max, Math.Max(Min, current + diff));
-			UpdateValue(newValue, newValue);
-
-			ClearOnKeyDownTimer();
-			SetOnKeyDownTimer();
-		}
-
-		public async Task Focus()
-		{
-			ElementReference reference = slideBox;
-			if (reference.Id != null)
-				await reference.FocusAsync();
-		}
-
-		private void ClearOnKeyDownTimer()
-		{
-			timer.Stop();
-		}
-
-		private void SetOnKeyDownTimer()
-		{
-			timer.Start();
-		}
-
-		private string GetAriaValueText(double value)
-		{
-			if (AriaValueText != null)
-				return AriaValueText(value);
-			else
-				return value.ToString();
-		}
-
-		[JSInvokable]
-		public void MouseOrTouchMove(ManualRectangle rect, double horizontalPosition, double verticalPosition)
-		{
-			//Debug.WriteLine($"rect:{rect.left} {rect.right}  horiz: {horizontalPosition}");
-			double steps = (Max - Min) / Step;
-			//Debug.WriteLine($"steps: {steps}");
-			double sliderLength = Vertical ? rect.Height : rect.Width;
-			//Debug.WriteLine($"sliderLength: {sliderLength}");
-			double stepLength = sliderLength / steps;
-			//Debug.WriteLine($"stepLength: {stepLength}");
-
-			double distance;
-			double currentSteps;
-
-			if (!Vertical)
-			{
-				distance = horizontalPosition - rect.Left;
-			}
-			else
-			{
-				distance = rect.Bottom - verticalPosition;
-			}
-			//Debug.WriteLine($"distance: {distance}");
-			currentSteps = distance / stepLength;
-			//Debug.WriteLine($"currentSteps: {currentSteps}");
-
-			double currentValue;
-			double renderedValue;
-
-			if (currentSteps > Math.Floor(steps))
-			{
-				renderedValue = currentValue = Max;
-			}
-			else if (currentSteps < 0)
-			{
-				renderedValue = currentValue = Min;
-			}
-			else
-			{
-				renderedValue = Min + Step * currentSteps;
-				currentValue = Min + Step * Math.Round(currentSteps);
-			}
-
-			UpdateValue(currentValue, renderedValue);
-
-		}
-
-		[JSInvokable]
-		public void MouseOrTouchEnd()
-		{
-			_renderedValue = value;  // _renderedValue is only different during mouse move... falls back to Value when done.
-			UpdateState();
-			_ = ValueChanged.InvokeAsync(value);
-
-			shouldFocus = true;
-		}
-
-		public void UpdateValue(double value, double renderedValue)
-		{
-			int numDec = 0;
-			if (!double.IsInfinity(Step!))
-			{
-				while (Math.Round(Step * Math.Pow(10, numDec)) / Math.Pow(10, numDec) != Step)
-				{
-					numDec++;
-				}
-			}
+        private static readonly Dictionary<string, string> GlobalClassNames = new()
+        {
+            { "root", "ms-Slider" },
+            { "enabled", "ms-Slider-enabled" },
+            { "disabled", "ms-Slider-disabled" },
+            { "row", "ms-Slider-row" },
+            { "column", "ms-Slider-column" },
+            { "container", "ms-Slider-container" },
+            { "slideBox", "ms-Slider-slideBox" },
+            { "line", "ms-Slider-line" },
+            { "thumb", "ms-Slider-thumb" },
+            { "activeSection", "ms-Slider-active" },
+            { "inactiveSection", "ms-Slider-inactive" },
+            { "valueLabel", "ms-Slider-value" },
+            { "showValue", "ms-Slider-showValue" },
+            { "showTransitions", "ms-Slider-showTransitions" },
+            { "zeroTick", "ms-Slider-zeroTick" },
+            { "titleLabel", "ms-Slider-titleLabel" },
+            { "lineContainer", "ms-Slider-lineContainer" }
+        };
+        private bool shouldFocus;
 
 
-			// Make sure value has correct number of decimal places based on number of decimals in step
-			double roundedValue = double.Parse(value.ToString($"F{numDec}"));
-			bool valueChanged = roundedValue != value;
+        private void UpdateState()
+        {
+            thumbOffsetPercent = Min == Max ? 0 : ((_renderedValue - Min) / (Max - Min)) * 100.0;
 
-			if (SnapToStep)
-			{
-				renderedValue = roundedValue;
-			}
+            zeroOffsetPercent = Min >= 0 ? 0 : (-Min / (Max - Min)) * 100;
+            //lengthString = (set as property)
+            showTransitions = _renderedValue == value;
+            StateHasChanged();
+        }
 
-			this.value = roundedValue;
-			_renderedValue = renderedValue;
-			UpdateState();
-			if (valueChanged)
-			{
-				_ = ValueChanged.InvokeAsync(value);
-			}
-		}
+        protected override async Task OnInitializedAsync()
+        {
+            timer.Interval = 1000;
+            timer.AutoReset = false;
+            timer.Elapsed += Timer_Elapsed;
+            await base.OnInitializedAsync();
+        }
 
-		private static string GetStyleUsingOffsetPercent(bool vertical, double thumbOffsetPercent)
-		{
-			string? direction = vertical ? "bottom" : "left";  // skipping RTL
-			return $"{direction}:{thumbOffsetPercent}%;";
-		}
+        private void Timer_Elapsed(object? sender, ElapsedEventArgs e)
+        {
+            _ = InvokeAsync(() => _ = ValueChanged.InvokeAsync(value));
+        }
 
-		public override async ValueTask DisposeAsync()
-		{
-			try
-			{
-				if (scriptModule != null)
-				{
-					await scriptModule.InvokeVoidAsync("unregisterHandlers", selfReference);
-					await scriptModule.DisposeAsync();
-				}
+        protected override async Task OnParametersSetAsync()
+        {
+            if (Value.HasValue)
+            {
+                value = Value.Value;
+                _renderedValue = value;
+            }
+            else if (DefaultValue.HasValue && !initialValueSet)
+            {
+                initialValueSet = true;
+                value = DefaultValue.Value;
+                _renderedValue = value;
+            }
 
-				selfReference?.Dispose();
+            //if  (selfReference == null)
+            //    selfReference = DotNetObjectReference.Create(this);
+            //if (Disabled)
+            //    await baseModule!.InvokeVoidAsync("unregisterHandler", selfReference);
+            //else
+            //    await baseModule!.InvokeVoidAsync("registerMouseOrTouchStart", selfReference, slideBox, sliderLine);
 
-				await base.DisposeAsync();
-			}
-			catch (TaskCanceledException)
-			{
-			}
-		}
-	}
+            UpdateState();
+
+            await base.OnParametersSetAsync();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (scriptModule == null)
+                scriptModule = await JSRuntime!.InvokeAsync<IJSObjectReference>("import", ScriptPath);
+            selfReference = DotNetObjectReference.Create(this);
+
+            if (firstRender)
+            {
+                if (!Disabled)
+                {
+                    await scriptModule.InvokeVoidAsync("registerMouseOrTouchStart", selfReference, slideBox, sliderLine);
+                }
+                else
+                    await scriptModule.InvokeVoidAsync("unregisterHandlers", selfReference);
+            }
+
+            if (shouldFocus)
+            {
+                shouldFocus = false;
+                await Focus();
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        [JSInvokable]
+        public void OnKeyDown(KeydownSliderArgs args)
+        {
+            double diff = 0;
+            double current = value;
+            if (args.Min)
+            {
+                current = Min;
+            }
+            else if (args.Max)
+            {
+                current = Max;
+            }
+            else
+            {
+                diff = args.Step;
+            }
+
+            double newValue = Math.Min(Max, Math.Max(Min, current + diff));
+            UpdateValue(newValue, newValue);
+
+            ClearOnKeyDownTimer();
+            SetOnKeyDownTimer();
+        }
+
+        public async Task Focus()
+        {
+            ElementReference reference = slideBox;
+            if (reference.Id != null)
+                await reference.FocusAsync();
+        }
+
+        private void ClearOnKeyDownTimer()
+        {
+            timer.Stop();
+        }
+
+        private void SetOnKeyDownTimer()
+        {
+            timer.Start();
+        }
+
+        private string GetAriaValueText(double value)
+        {
+            if (AriaValueText != null)
+                return AriaValueText(value);
+            else
+                return value.ToString();
+        }
+
+        [JSInvokable]
+        public void MouseOrTouchMove(ManualRectangle rect, double horizontalPosition, double verticalPosition)
+        {
+            //Debug.WriteLine($"rect:{rect.left} {rect.right}  horiz: {horizontalPosition}");
+            double steps = (Max - Min) / Step;
+            //Debug.WriteLine($"steps: {steps}");
+            double sliderLength = Vertical ? rect.Height : rect.Width;
+            //Debug.WriteLine($"sliderLength: {sliderLength}");
+            double stepLength = sliderLength / steps;
+            //Debug.WriteLine($"stepLength: {stepLength}");
+
+            double distance;
+            double currentSteps;
+
+            if (!Vertical)
+            {
+                distance = horizontalPosition - rect.Left;
+            }
+            else
+            {
+                distance = rect.Bottom - verticalPosition;
+            }
+            //Debug.WriteLine($"distance: {distance}");
+            currentSteps = distance / stepLength;
+            //Debug.WriteLine($"currentSteps: {currentSteps}");
+
+            double currentValue;
+            double renderedValue;
+
+            if (currentSteps > Math.Floor(steps))
+            {
+                renderedValue = currentValue = Max;
+            }
+            else if (currentSteps < 0)
+            {
+                renderedValue = currentValue = Min;
+            }
+            else
+            {
+                renderedValue = Min + Step * currentSteps;
+                currentValue = Min + Step * Math.Round(currentSteps);
+            }
+
+            UpdateValue(currentValue, renderedValue);
+
+        }
+
+        [JSInvokable]
+        public void MouseOrTouchEnd()
+        {
+            _renderedValue = value;  // _renderedValue is only different during mouse move... falls back to Value when done.
+            UpdateState();
+            _ = ValueChanged.InvokeAsync(value);
+
+            shouldFocus = true;
+        }
+
+        public void UpdateValue(double value, double renderedValue)
+        {
+            int numDec = 0;
+            if (!double.IsInfinity(Step!))
+            {
+                while (Math.Round(Step * Math.Pow(10, numDec)) / Math.Pow(10, numDec) != Step)
+                {
+                    numDec++;
+                }
+            }
+
+
+            // Make sure value has correct number of decimal places based on number of decimals in step
+            double roundedValue = double.Parse(value.ToString($"F{numDec}"));
+            bool valueChanged = roundedValue != value;
+
+            if (SnapToStep)
+            {
+                renderedValue = roundedValue;
+            }
+
+            this.value = roundedValue;
+            _renderedValue = renderedValue;
+            UpdateState();
+            if (valueChanged)
+            {
+                _ = ValueChanged.InvokeAsync(value);
+            }
+        }
+
+        private static string GetStyleUsingOffsetPercent(bool vertical, double thumbOffsetPercent)
+        {
+            string? direction = vertical ? "bottom" : "left";  // skipping RTL
+            return $"{direction}:{thumbOffsetPercent}%;";
+        }
+
+        public override async ValueTask DisposeAsync()
+        {
+            try
+            {
+                if (scriptModule != null)
+                {
+                    await scriptModule.InvokeVoidAsync("unregisterHandlers", selfReference);
+                    await scriptModule.DisposeAsync();
+                }
+
+                selfReference?.Dispose();
+
+                await base.DisposeAsync();
+            }
+            catch (TaskCanceledException)
+            {
+            }
+        }
+    }
 }
